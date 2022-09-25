d726f86a9089b4440ce3f327cc701c98a2e04283
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-09-25 14:20:48 +0400

    test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import 'normalize.css'
 import './styles/main.scss';
+
+const headerDomElement = document.querySelector(".header");
+const headerSiteNav = document.querySelector(".header__nav");
+const burgerButton = document.querySelector(".header__menu");
+
+//выпадающее меню сайта
+function toggleSiteNav() {
+  headerSiteNav.classList.toggle("header__nav_active");
+  burgerButton.classList.toggle("header__menu_active");
+}
+
+burgerButton.addEventListener("click", toggleSiteNav);
+
+//если нажали мимо меню хедера,то попап закроется
+
+document.addEventListener("click", (e) => {
+  if (
+    !e.path.includes(headerDomElement) &&
+    headerSiteNav.classList.contains("header__nav_active")
+  ) {
+    toggleSiteNav();
+  }
+})
